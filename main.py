9cbc70ead87089729832b3f8a07a5889583eb899
MichavR <michalrdz@gmail.com>
2022-01-02 23:26:39 +0100

    changed obstacles, added plane obj, plane descending, steering, start delay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-import pygame
 import os
+import pygame
+import random
+import time
+
 
 pygame.init()
 
@@ -18,7 +20,15 @@ def texts(txt, x, y, size):  # ingame texts function, default position: middle
     screen.blit(txt_render, (x, y))
 
 
-current_display = "gameplay"
+def counter():
+    t = 3
+    while t:
+        time.sleep(1)
+        t -= 1
+    return t
+
+
+current_display = "get_ready"
 
 
 class Obstacle:
@@ -26,11 +36,11 @@ class Obstacle:
         self.x = x
         self.width = width
         self.y_upper = 0
-        self.height_upper = random.randint(150, 250)
-        self.space = 150
+        self.height_upper = random.randint(250, 500)
+        self.space = 290
         self.y_lower = self.height_upper + self.space
-        self.height_lower = screen_height - self.y_lower
-        self.color = (3, 252, 132)
+        self.height_lower = (screen_height / 0.5) - self.y_lower
+        self.color = (0, 102, 153)
         self.shape_upper = pygame.Rect(self.x, self.y_upper, self.width, self.height_upper)
         self.shape_lower = pygame.Rect(self.x, self.y_lower, self.width, self.height_lower)
 
@@ -44,21 +54,62 @@ class Obstacle:
         self.shape_lower = pygame.Rect(self.x, self.y_lower, self.width, self.height_lower)
 
 
+class Plane:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 100
+        self.height = 77
+        self.shape = pygame.Rect(self.x, self.y, self.width-5, self.height-5)
+        self.pic = pygame.image.load(os.path.join('img/plane.png'))
+
+    def draw_plane(self):
+        screen.blit(self.pic, (self.x, self.y))
+
+    def plane_movement(self, v):
+        self.y = self.y + v
+
+
 obstacles = []
 
 for i in range(21):
     obstacles.append(Obstacle(i * screen_width / 20, screen_width / 20))
 
+player = Plane(620, 475)
+dy = 0
+points = 0
+
 while True:  # game's main loop
     for event in pygame.event.get():
+        dy = +0.23
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                dy = -0.5
+            if event.key == pygame.K_DOWN:
+                dy = +0.5
+
     screen.fill((0, 0, 0))
     if current_display == "menu":  # game's main screen
         texts("Press SPACE to begin", 500, 780, 30)
-        game_logo = pygame.image.load(os.path.join("logo1.png"))
+        game_logo = pygame.image.load(os.path.join("img/logo1.png"))
         screen.blit(game_logo, (450, 200))
+
+    elif current_display == "get_ready":
+        for p in obstacles:
+            p.draw_obstacle()
+        for p in obstacles:
+            if p.x <= - p.width:
+                obstacles.remove(p)
+                obstacles.append(Obstacle(screen_width, screen_width/20))
+
+        player.draw_plane()
+        pygame.display.update()
+        if counter() == 0:
+            current_display = "gameplay"
+
     elif current_display == "gameplay":
         for p in obstacles:
             p.obstacle_movement(1)
@@ -66,6 +117,9 @@ while True:  # game's main loop
         for p in obstacles:
             if p.x <= - p.width:
                 obstacles.remove(p)
-                obstacles.append(Obstacle(screen_width, screen_width/20))
+                obstacles.append(Obstacle(screen_width, screen_width / 20))
+
+        player.draw_plane()
+        player.plane_movement(dy)
 
     pygame.display.update()
